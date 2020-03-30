1bd5bd163038c5a7802f3d34123cbc19615ce6ce
ramon96 <rfm1@live.nl>
2020-03-30 19:52:00 +0200

    little styling details

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Design your own shirt'});
+  res.render('index', { title: 'RF Design'});
 });
 
 
